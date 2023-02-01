ca013d91b54711924fe70d906971e4e23e69d7db
Cameron Chandler <webstormcam@gmail.com>
2023-01-31 22:04:47 -0500

    Got the items to render

--- script.js
+++ script.js
@@ -1,9 +1,24 @@
 import { menuArray } from './data.js'
 
 function renderMenu(){
+    const menu = document.getElementById('menu')
+    let options =''
     for(let food of menuArray){
+   options+=`<div class="menu-item">
+   <div class="food-info">
+   <div class="icon">${food.emoji}</div>
+   <div class="food-desc">
+        <h2>${food.name}</h2>
+        <p>${food.ingredients}</p>
+        <p>${food.price}</p>
+   </div>
+   </div>
+   <div class="add-food"></div>
    
+   
+   </div>`
     }
+    menu.innerHTML+=options
 }
 
 
